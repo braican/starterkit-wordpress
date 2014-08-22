a0914905882b04a98110444dc80d1504550a66d7
braican <nick.braica@gmail.com>
2014-08-22 15:46:15 -0400

    register post types and taxonomies

--- webroot/wp-content/themes/_s/functions.php
+++ webroot/wp-content/themes/_s/functions.php
@@ -9,7 +9,7 @@
  * Set the content width based on the theme's design and stylesheet.
  */
 if ( ! isset( $content_width ) ) {
-	$content_width = 640; /* pixels */
+    $content_width = 640; /* pixels */
 }
 
 if ( ! function_exists( '_s_setup' ) ) :
@@ -22,48 +22,48 @@ if ( ! function_exists( '_s_setup' ) ) :
  */
 function _s_setup() {
 
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on _s, use a find and replace
-	 * to change '_s' to the name of your theme in all the template files
-	 */
-	load_theme_textdomain( '_s', get_template_directory() . '/languages' );
+    /*
+     * Make theme available for translation.
+     * Translations can be filed in the /languages/ directory.
+     * If you're building a theme based on _s, use a find and replace
+     * to change '_s' to the name of your theme in all the template files
+     */
+    load_theme_textdomain( '_s', get_template_directory() . '/languages' );
 
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+    // Add default posts and comments RSS feed links to head.
+    add_theme_support( 'automatic-feed-links' );
 
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 */
-	add_theme_support( 'post-thumbnails' );
+    /*
+     * Enable support for Post Thumbnails on posts and pages.
+     */
+    add_theme_support( 'post-thumbnails' );
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', '_s' ),
-	) );
-	
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
-	) );
+    // This theme uses wp_nav_menu() in one location.
+    register_nav_menus( array(
+        'primary' => __( 'Primary Menu', '_s' ),
+    ) );
+    
+    /*
+     * Switch default core markup for search form, comment form, and comments
+     * to output valid HTML5.
+     */
+    add_theme_support( 'html5', array(
+        'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+    ) );
 
-	/*
-	 * Enable support for Post Formats.
-	 * See http://codex.wordpress.org/Post_Formats
-	 */
-	add_theme_support( 'post-formats', array(
-		'aside', 'image', 'video', 'quote', 'link'
-	) );
+    /*
+     * Enable support for Post Formats.
+     * See http://codex.wordpress.org/Post_Formats
+     */
+    add_theme_support( 'post-formats', array(
+        'aside', 'image', 'video', 'quote', 'link'
+    ) );
 
-	// Setup the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( '_s_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
+    // Setup the WordPress core custom background feature.
+    add_theme_support( 'custom-background', apply_filters( '_s_custom_background_args', array(
+        'default-color' => 'ffffff',
+        'default-image' => '',
+    ) ) );
 }
 endif; // _s_setup
 add_action( 'after_setup_theme', '_s_setup' );
@@ -74,15 +74,15 @@ add_action( 'after_setup_theme', '_s_setup' );
  * @link http://codex.wordpress.org/Function_Reference/register_sidebar
  */
 function _s_widgets_init() {
-	register_sidebar( array(
-		'name'          => __( 'Sidebar', '_s' ),
-		'id'            => 'sidebar-1',
-		'description'   => '',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	) );
+    register_sidebar( array(
+        'name'          => __( 'Sidebar', '_s' ),
+        'id'            => 'sidebar-1',
+        'description'   => '',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h1 class="widget-title">',
+        'after_title'   => '</h1>',
+    ) );
 }
 add_action( 'widgets_init', '_s_widgets_init' );
 
@@ -90,15 +90,15 @@ add_action( 'widgets_init', '_s_widgets_init' );
  * Enqueue scripts and styles.
  */
 function _s_scripts() {
-	wp_enqueue_style( '_s-style', get_stylesheet_uri() );
+    wp_enqueue_style( '_s-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( '_s-plugins', get_template_directory_uri() . '/js/plugins.js', array('jquery'), '20140725', true );
+    wp_enqueue_script( '_s-plugins', get_template_directory_uri() . '/js/plugins.js', array('jquery'), '20140725', true );
 
-	wp_enqueue_script( '_s-main', get_template_directory_uri() . '/js/main.js', array('plugins', 'jquery'), '20140725', true );
+    wp_enqueue_script( '_s-main', get_template_directory_uri() . '/js/main.js', array('plugins', 'jquery'), '20140725', true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
 }
 add_action( 'wp_enqueue_scripts', '_s_scripts' );
 
@@ -123,9 +123,41 @@ require get_template_directory() . '/inc/extras.php';
 require get_template_directory() . '/inc/customizer.php';
 
 
-/* --------------------------
- * _s customizations
- * -------------------------- */
+/* --------------------------------------------
+ * --_s content types and taxonomies
+ * -------------------------------------------- */
+
+/**
+ * _s_content_types
+ *
+ * http://codex.wordpress.org/Function_Reference/register_post_type
+ */
+function _s_content_types(){
+
+    // register_post_type('type_name', array(
+
+    // ));
+}
+add_action('init', '_s_content_types');
+
+
+/**
+ * _s_taxonomies
+ *
+ * http://codex.wordpress.org/Function_Reference/register_taxonomy
+ */
+function _s_taxonomies(){
+
+    // register_taxonomy('taxonomy_name', array('content_types'), array(
+
+    // ));
+}
+add_action('init', '_s_taxonomies');
+
+
+/* --------------------------------------------
+ * --_s customizations
+ * -------------------------------------------- */
 
 //
 // --structure
