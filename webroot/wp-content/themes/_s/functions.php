8d7b0c2fcb56754f11c8855f9f0effb236ca615c
braican <nick.braica@gmail.com>
2015-04-07 16:58:16 -0400

    some comments and examples, as well as a couple filters

--- webroot/wp-content/themes/_s/functions.php
+++ webroot/wp-content/themes/_s/functions.php
@@ -236,6 +236,12 @@ function _s_the_page_blocks(){
  * --filters
  * -------------------------------------------- */
 
+
+//
+// wysiwyg stuff
+//
+
+
 /**
  * _s_modify_wysiwyg
  * @param $init : the object that drives the wysiwyg
@@ -246,6 +252,81 @@ function _s_modify_wysiwyg( $init ) {
     return $init;
 }
 add_filter('tiny_mce_before_init', '_s_modify_wysiwyg');
+
+
+
+
+/**
+ * _s_add_style_select
+ *
+ * filter to add a style select dropdown to the WYSIWYG editor
+ */
+function _s_add_style_select( $buttons ) {
+    array_unshift( $buttons, 'styleselect' );
+    return $buttons;
+}
+add_filter('mce_buttons_2', '_s_add_style_select');
+
+
+
+
+/**
+ * _s_custom_wysiwyg_classes
+ *
+ * filter to add a the actual styles to the wysiwyg
+ */
+function _s_custom_wysiwyg_classes( $init_array ) {  
+    // Define the style_formats array
+    $style_formats = array(  
+        // Each array child is a format with it's own settings
+        array(  
+            'title' => '.translation',  
+            'block' => 'blockquote',  
+            'classes' => 'translation',
+            'wrapper' => true,
+        ),
+    );
+    // Insert the array, JSON ENCODED, into 'style_formats'
+    $init_array['style_formats'] = json_encode( $style_formats );  
+    
+    return $init_array;  
+  
+} 
+add_filter( 'tiny_mce_before_init', '_s_custom_wysiwyg_classes' );  
+
+/**
+ * style format arguments
+ * https://codex.wordpress.org/TinyMCE_Custom_Styles
+ *
+ * inline     – Name of the inline element to produce for example “span”. The current text selection will be wrapped in this inline element.
+ * block      – Name of the block element to produce for example “h1″. Existing block elements within the selection gets replaced with the new block element.
+ * selector   – CSS 3 selector pattern to find elements within the selection by. This can be used to apply classes to specific elements or complex things like odd rows in a table. Note that if you combine both selector and block then you can get more nuanced behavior where the button changes the class of the selected tag by default, but adds the block tag around the cursor if the selected tag isn't found.
+ * classes    – Space separated list of classes to apply to the selected elements or the new inline/block element.
+ * styles     – Name/value object with CSS style items to apply such as color etc.
+ * attributes – Name/value object with attributes to apply to the selected elements or the new inline/block element.
+ * exact      – Disables the merge similar styles feature when used. This is needed for some CSS inheritance issues such as text-decoration for underline/strikethrough.
+ * wrapper    – State that tells that the current format is a container format for block elements. For example a div wrapper or blockquote.
+ */
+
+
+//
+// filters for rendering fields
+//
+
+/**
+ * _s_create_img_markup 
+ * 
+ * filter to render an image from a custom field
+ * @param $img_obj
+ */
+function _s_create_img_markup($img_obj){
+
+    return '<img src="' . $img_obj['url'] . '">';
+
+}
+add_filter('_s_image_markup', '_s_create_img_markup');
+
+
 
 
 
