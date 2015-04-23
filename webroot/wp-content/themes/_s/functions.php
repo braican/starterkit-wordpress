8b45885975103686000973211f57a22d1c49b71b
braican <nick.braica@gmail.com>
2015-04-23 14:44:20 -0400

    thumbnail regeneration plugin, plus custom post order plugin, plus function to allow upscaling

--- webroot/wp-content/themes/_s/functions.php
+++ webroot/wp-content/themes/_s/functions.php
@@ -45,6 +45,8 @@ function _s_setup() {
      * Enable support for Post Thumbnails on posts and pages.
      */
     add_theme_support( 'post-thumbnails' );
+    // add theme sizes
+    // add_image_size('thumbnail-size', 368, 272, true);   
 
     // This theme uses wp_nav_menu() in one location.
     register_nav_menus( array(
@@ -341,6 +343,36 @@ function _s_create_img_markup($img_obj){
 }
 add_filter('_s_image_markup', '_s_create_img_markup');
 
+
+
+
+//
+// thumbnails and scaline
+//
+
+
+/**
+ * _s_image_crop_dimensions
+ *
+ * filter to allow the upscaling of images to fit their assigned dimensions
+ * http://wordpress.stackexchange.com/questions/50649/how-to-scale-up-featured-post-thumbnail
+ */
+function _s_image_crop_dimensions($default, $orig_w, $orig_h, $new_w, $new_h, $crop) {
+    if(!$crop)
+        return null; // let the wordpress default function handle this
+
+    $aspect_ratio = $orig_w / $orig_h;
+    $size_ratio = max($new_w / $orig_w, $new_h / $orig_h);
+
+    $crop_w = round($new_w / $size_ratio);
+    $crop_h = round($new_h / $size_ratio);
+
+    $s_x = floor( ($orig_w - $crop_w) / 2 );
+    $s_y = floor( ($orig_h - $crop_h) / 2 );
+
+    return array( 0, 0, (int) $s_x, (int) $s_y, (int) $new_w, (int) $new_h, (int) $crop_w, (int) $crop_h );
+}
+// add_filter('image_resize_dimensions', '_s_image_crop_dimensions', 10, 6);
 
 
 
