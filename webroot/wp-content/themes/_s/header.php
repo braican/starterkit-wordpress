daa7863c216a447979167a5f8c8370f92a241a38
braican <nick.braica@gmail.com>
2015-12-01 17:45:52 -0500

    home url

--- webroot/wp-content/themes/_s/header.php
+++ webroot/wp-content/themes/_s/header.php
@@ -25,7 +25,7 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-logo">
 			<?php if( $logo = get_theme_mod('_s_logo') ) : ?>
-				<img src="<?php echo $logo; ?>">
+				<a href="<?php echo esc_url( home_url('/') ); ?>"><img src="<?php echo $logo; ?>"></a>
 			<?php endif; ?>
 		</div>
 		<div class="site-branding">
